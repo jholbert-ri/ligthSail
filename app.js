d962994e28f167813411e6cef159fbc303241efc
jholbert-ri <jcaro@ridery.app>
2025-03-31 15:50:40 -0400

    feat: adding deploy action on main branch 8

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get("/status", (req, res) => {
-  res.send(`Hola desde Lightsail - ${new Date()}`);
+  res.send(`Hola desde Lightsail ***2*** - ${new Date()}`);
 });
 
 app.listen(port, () => {
