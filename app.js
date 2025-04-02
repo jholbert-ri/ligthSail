5748670bd6c40f53d60920ec6d284329805a65c6
jholbert-ri <jcaro@ridery.app>
2025-04-02 13:08:56 -0400

    modifify app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get("/status", (req, res) => {
   res.send(`Hola desde Lightsail ***2*** - ${new Date()}`);
 });
 
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
